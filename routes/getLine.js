7d2aca8a118de45e9ef193362db4eade66a8304e
BB <1036012184@qq.com>
2021-01-31 15:19:36 +0800

    面积热力图、折现图数据接口完成

--- routes/getLine.js
+++ routes/getLine.js
@@ -4,30 +4,52 @@ const moment = require('moment')
 
 var router = express.Router()
 
-router.get('/todo/getHeat', async (req, res) => {
+router.get('/todo/getLine', async (req, res) => {
   const Date = '2021-01-11'
   var createDate = moment(Date + ' 00:00', 'YYYY-MM-DD HH:mm').unix()
-  var endDate = moment(Date + ' 02:00', 'YYYY-MM-DD HH:mm').unix()
+  var endDate = moment(Date + ' 03:00', 'YYYY-MM-DD HH:mm').unix()
   var countList = new Array
-  // var arr2 = new Array
-  var dayData = new Array
   var weekData = new Array
 
-  for (let i = 0; i < 12; i = i + 2) {
+  for (let i = 0; i < 56; i = i + 3) {
     createDate = createDate + i * 3600
     endDate = endDate + i * 3600
-    countList[i / 2] = await List
+    countList[i / 3] = await List
       .countDocuments({ createDate: { $gt: createDate.toString(), $lte: endDate.toString() } })
-    // arr2[i / 7200] = await List
-    //   .find({ createDate: { $gt: createDate.toString(), $lte: endDate.toString() } }, 'isFinish -_id')
   }
 
-  for (let j = 0; j < 12; j++) {
-    dayData.push({
-      endDate: Date + ' ' + (j * 2) + ':00',
-      value: countList[j],
-      tag: countList[j] == 0 ? 0 : 1
-    })
+  for (let j = 0; j < 7; j++) {
+    let week
+    switch (j) {
+      case 0:
+        week = 'Mon'
+        break
+      case 1:
+        week = 'Tue'
+        break
+      case 2:
+        week = 'Wed'
+        break
+      case 3:
+        week = 'Thu'
+        break
+      case 4:
+        week = 'Fri'
+        break
+      case 5:
+        week = 'Sta'
+        break
+      case 6:
+        week = 'Sun'
+        break
+    }
+    for (let z = 0; z < 8; z++) {
+      weekData.push({
+        week: week,
+        hour: z * 3,
+        value: countList[z]
+      })
+    }
   }
 
   List
@@ -36,7 +58,7 @@ router.get('/todo/getHeat', async (req, res) => {
       res.status(200).json({
         success: true,
         message: 'success',
-        data: dayData
+        data: weekData
       })
     })
     .catch(err => {
